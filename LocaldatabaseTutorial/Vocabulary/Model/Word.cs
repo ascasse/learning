65236837ead3cec61fae49ebef041d83f8e0b91e
Antonio Simón <ascasse@gmail.com>
2021-01-02 12:55:17 +0100

    Added vocabulary module

---
+++ LocaldatabaseTutorial/Vocabulary/Model/Word.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+
+namespace Learning.Model
+{
+    [Table("Words")]
+    public class Word
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime LastUse { get; set; }
+        public int Views { get; set; }
+        [ForeignKey(typeof(Category))]
+        public int CategoryId { get; set; }
+    }
+}
